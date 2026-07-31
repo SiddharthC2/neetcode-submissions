52ac5a8d5e9f9b761e73d9618d0d2e2f3af3e8b3
SiddharthC2 <123933245+SiddharthC2@users.noreply.github.com>
2026-07-31 09:03:29 +0530

    Add: reverse-nodes-in-k-group - submission-1

---
+++ Data Structures & Algorithms/reverse-nodes-in-k-group/submission-1.java
@@ -0,0 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+
+    private void reverseListGrp(ListNode startNode, ListNode endNode) {
+        ListNode curr = startNode, currNxt = curr.next, currNxtNxt;
+        while (curr != endNode) {
+            currNxtNxt = currNxt.next;
+            currNxt.next = curr;
+            curr = currNxt;
+            currNxt = currNxtNxt;
+        }
+    }
+
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if (k == 1) {
+            return head;
+        }
+        ListNode newHead = new ListNode();
+        newHead.next = head;
+        ListNode prevGrpTail, currGrpHead, nextGrpHead, curr;
+        prevGrpTail = newHead;
+        currGrpHead = newHead.next;
+        curr = currGrpHead;
+        int currGrpCount = 1;
+        while (curr != null) {
+            if (currGrpCount == k) {
+                nextGrpHead = curr.next;
+                reverseListGrp(currGrpHead, curr);
+                prevGrpTail.next = curr;
+                prevGrpTail = currGrpHead;
+                currGrpHead.next = nextGrpHead;
+                currGrpHead = nextGrpHead;
+                curr = currGrpHead;
+                currGrpCount = 1;
+            } else {
+                curr = curr.next;
+                currGrpCount++;
+            }
+        }
+        return newHead.next;
+    }
+}
