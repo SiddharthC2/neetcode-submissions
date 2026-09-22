72f2db1eda7d3a13cdbb3a8a99eb206015956631
SiddharthC2 <123933245+SiddharthC2@users.noreply.github.com>
2026-09-22 11:03:51 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int currMin = prices[0], maxProfit = 0;
+        for (int i=1; i<prices.length; i++) {
+            maxProfit = Math.max(maxProfit, prices[i] - currMin);
+            currMin = Math.min(currMin, prices[i]);
+        }
+        return maxProfit;
+    }
+}
